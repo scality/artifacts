dfe83ae3b4634c90d01eb610cd170a482e862a18
Sweenu <brunoinec@gmail.com>
2017-07-13 02:25:31 +0200

    what he said

---
+++ tests/util/stream/test_archive_stream.py
@@ -0,0 +1,11 @@
+import unittest
+import tarfile
+
+from .archive_stream import streamed_archive
+
+
+class ArchiveStreamTestCase(unittest.TestCase):
+
+    def test_streamed_archive(self):
+        with tarfile.open(fileobj=streamed_archive(b'toto'), mode='r:gz') as f:
+            self.assertEqual(f.extractfile('test_file').read(), b'toto')
