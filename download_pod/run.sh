e7d859cbbf1998b859be34a65e3607bd1a112029
Sylvain Killian <sylvain.killian@scality.com>
2018-06-04 10:37:28 +0200

    DEBUG update to more permanent test artifact

--- download_pod/run.sh
+++ download_pod/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-URL="http://artifacts/builds/bitbucket:scality:ring:dev-staging-8.0.0.0.r180523081704.a66e0ba.pre-merge.00000723/repo/centos6/scality/thirdparty/grafana-4.3.2-1.x86_64.rpm"
+URL="http://artifacts/builds/bitbucket:scality:ring:promoted-7.4.0.0/repo/centos6/scality/thirdparty/grafana-4.3.2-1.x86_64.rpm"
 
 
 while true; do
     curl -vvv $URL > grafana.rpm
     if [ $? != 0 ]; then
-	echo 'FAILURE'
+        echo 'FAILURE'
     fi
 done
