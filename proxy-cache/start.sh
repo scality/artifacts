2567423143893f2a01482ac2d9b66eb536fd88ff
Thomas Carmet <thomas.carmet@scality.com>
2019-05-15 16:28:44 +0200

    PIPDEP-756 add option to deactivate caching

--- proxy-cache/start.sh
+++ proxy-cache/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst '${NGINX_LISTEN_PORT} ${ARTIFACTS_HOST}' \
+envsubst '${NGINX_LISTEN_PORT} ${ARTIFACTS_HOST} ${NGINX_NO_CACHE}'  \
   < /etc/nginx/conf.d/artifacts.conf.template \
   > /etc/nginx/conf.d/default.conf && exec nginx -g 'daemon off;'
