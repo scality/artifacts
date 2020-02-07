2758a58287caf27474b18e141e93a6b20dc78f01
Thomas Carmet <thomas.carmet@scality.com>
2020-02-07 17:34:37 +0100

    RELENG-3545 add ci tests to artifacts

---
+++ tests/end2end/conftest.py
@@ -0,0 +1,33 @@
+
+
+import boto3
+import os
+import pytest
+
+@pytest.fixture(scope="class")
+def s3_client(request):
+    session = boto3.session.Session()
+    s3_client = session.client(
+        service_name='s3',
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID', 'accessKey1'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY', 'verySecretKey1'),
+        endpoint_url=os.getenv('ENDPOINT_URL', 'http://cloudserver-front:8000')
+    )
+    request.cls.s3_client = s3_client
+
+@pytest.fixture(scope="class")
+def artifacts_url(request):
+    request.cls.artifacts_url = os.getenv('ARTIFACTS_URL', 'http://localhost')
+
+@pytest.fixture(scope="class")
+def container(request):
+    request.cls.container = 'githost:owner:repo:staging-0.0.0.0.r190101000000.1234567.pre-merge.12345678'
+
+@pytest.fixture(scope="class")
+def buckets(request):
+    buckets = (
+        'artifacts-staging',
+        'artifacts-promoted',
+        'artifacts-prolonged'
+    )
+    request.cls.buckets = buckets
