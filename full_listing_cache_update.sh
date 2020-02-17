f2d7cdaf89151df7d31c57f3476d7ddb7b9c8b46
Bertrand Demiddelaer <bertrand.demiddelaer@scality.com>
2020-02-17 15:55:09 +0100

    PIPDEP-853 use /data/nginx to store prefetch cache

--- full_listing_cache_update.sh
+++ full_listing_cache_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p /var/artifacts_full_listing_cache
-chmod 755 /var/artifacts_full_listing_cache
+mkdir -p /data/nginx/artifacts_full_listing_cache
+chmod 755 /data/nginx/artifacts_full_listing_cache
 
 while true
 do
@@ -11,10 +11,10 @@ done
 
 while true
 do
-  tmp_file=$(mktemp -p /var/artifacts_full_listing_cache)
+  tmp_file=$(mktemp -p /data/nginx/artifacts_full_listing_cache)
   chmod 644 ${tmp_file}
   date --utc > ${tmp_file}
-  curl --silent --fail --max-time 300 http://127.0.0.1/refresh_full_listing/ >> ${tmp_file} 2> /dev/null && mv -f ${tmp_file} /var/artifacts_full_listing_cache/listing
+  curl --silent --fail --max-time 300 http://127.0.0.1/refresh_full_listing/ >> ${tmp_file} 2> /dev/null && mv -f ${tmp_file} /data/nginx/artifacts_full_listing_cache/listing
   rm -f ${tmp_file}
   sleep 60
 done
