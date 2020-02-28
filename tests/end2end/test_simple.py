161800dcf0ce33dbc9a9434b109c6d25953e7322
Bertrand Demiddelaer <bertrand.demiddelaer@scality.com>
2020-02-28 17:16:23 +0100

    PIPDEP-862 add test for upload and copy behind ingress

--- tests/end2end/test_simple.py
+++ tests/end2end/test_simple.py
@@ -90,7 +90,29 @@ class TestSimple(unittest.TestCase):
 
         assert sha_download == sha_upload
 
+    def test_upload_and_copy_behind_ingress(self):
+
+        # Mimic an upload behind the ingress
+        url = '{artifacts_url}/upload/{container}/.final_status'.format(
+            artifacts_url=self.artifacts_url,
+            container=self.container
+        )
+        success = 'SUCCESSFUL'.encode('utf-8')
+        upload = requests.put(url, data=success, headers={'Script-Name': '/foo'})
+        assert upload.status_code == 403
+
+        # Mimic a copy behind the ingress
+        url = '{artifacts_url}/copy/{container}/copy_of_{container}/'.format(
+            artifacts_url=self.artifacts_url,
+            container=self.container
+        )
+        success = 'SUCCESSFUL'.encode('utf-8')
+        copy = requests.get(url, data=success, headers={'Script-Name': '/foo'})
+        assert copy.status_code == 403
+
     def test_simple_last_success_get_head(self):
+
+        # Test a direct upload
         url = '{artifacts_url}/upload/{container}/.final_status'.format(
             artifacts_url=self.artifacts_url,
             container=self.container
@@ -98,6 +120,8 @@ class TestSimple(unittest.TestCase):
         success = 'SUCCESSFUL'.encode('utf-8')
         upload = requests.put(url, data=success)
         assert upload.status_code == 200
+
+        # Check HEAD and GET on the object uploaded
         head = requests.head('{artifacts_url}/last_success/{container}'.format(
             artifacts_url=self.artifacts_url,
             container=self.container
