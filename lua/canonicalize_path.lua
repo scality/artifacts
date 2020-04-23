000003a656eecbf9ba998cdb6acc433bb3c39f6d
Christophe Meron <christophe.meron@scality.com>
2020-04-23 17:02:12 +0200

    PIPDEP-898 comments

--- lua/canonicalize_path.lua
+++ lua/canonicalize_path.lua
@@ -1,7 +1,14 @@
 local tmp = ngx.var.request_uri
+
+-- Remove query string
 tmp = tmp:gsub('?.*$', '', 1)
+
+-- Remove first part of path (command)
 tmp = tmp:gsub('^/[^/]+/', '', 1)
+
+-- substitute thoses chars with encoded string
 tmp = tmp:gsub('+', '%%2B')
 tmp = tmp:gsub('&amp;', '%%26')
+
 tmp = ngx.unescape_uri(tmp)
 return tmp
