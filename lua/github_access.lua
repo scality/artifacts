35d3b29531119883fa58361c09dbb840282b4f7c
Erwan BERNARD <erwan.bernard@scality.com>
2021-08-16 10:00:40 +0200

    Revert "[RELENG-4794] Set upload permision to specifique users"

--- lua/github_access.lua
+++ lua/github_access.lua
@@ -3,15 +3,8 @@
 -- Verify throw basic auth, the user is in the github company
 --
 --
-local github_api_enabled            = os.getenv('GITHUB_API_ENABLED')
-local github_api_company            = os.getenv('GITHUB_API_COMPANY')
-local env_github_restriction_upload = os.getenv('GITHUB_USER_ALLOWED_UPLOAD')
-local github_restriction_upload = {}
-
-for allowed_user in env_github_restriction_upload:gmatch("([^,]+)") do
-    table.insert(github_restriction_upload, allowed_user)
-    ngx.log(ngx.STDERR, allowed_user)
-end
+local github_api_enabled = os.getenv('GITHUB_API_ENABLED')
+local github_api_company = os.getenv('GITHUB_API_COMPANY')
 
 function wrong_credentials()
    ngx.header.content_type = 'text/plain'
@@ -32,6 +25,7 @@ end
 function authenticate(auth)
     divider = auth:find(':')
     local username = auth:sub(0, divider-1)
+    local resp = {}
 
     local res =  ngx.location.capture("/force_github_request/orgs/" .. github_api_company .. "/members/" .. username)
     ngx.log(ngx.STDERR, "status member \t\t" .. res.status)
@@ -61,33 +55,13 @@ function verify_header()
     return auth
 end
 
-function upload_checks()
-    ngx.log(ngx.STDERR, "upload check url is called ")
-    if string.sub(ngx.var.request_uri, 1,string.len("/upload/")) ~= "/upload/" then
-        return true
-    end
-    local res =  ngx.location.capture("/force_github_request/user")
-
-    username = res.body:match('"login": "([^/"]*)')
-    ngx.log(ngx.STDERR, username)
-    for _,v in pairs(github_restriction_upload) do
-        if v == username  then
-            return true
-        end
-    end
-    ngx.log(ngx.STDERR, 'User ' .. username .. 'not allowed to upload image')
-    return false
+auth = verify_header()
+if not auth then
+    return wrong_credentials()
 end
 
-if github_api_enabled == 'true' then
-    auth = verify_header()
-    if not auth then
-        return wrong_credentials()
-    end
-
-    local user = authenticate(auth)
-    ngx.log(ngx.STDERR, user)
-    if not user then
-        return not_allowed()
-    end
+local user = authenticate(auth)
+ngx.log(ngx.STDERR, user)
+if not user then
+    return not_allowed()
 end
