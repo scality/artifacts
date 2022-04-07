387f0b567a4b204980a0715e6171e96538d17215
Bertrand Demiddelaer <bertrand.demiddelaer@scality.com>
2022-04-07 08:47:25 +0200

    more checks

--- lua/version_object.lua
+++ lua/version_object.lua
@@ -5,19 +5,46 @@ local version, build, object = string.match(ngx.var.canonical_path, "^([^/]+)/([
 
 local url, res
 
+-- Check if the object is already present.
+--
+ngx.say('CHECK IF THE OBJECT IS ALREADY UPLOADED...')
+ngx.flush(true)
+url = "/force_real_request/download/" .. build .. "/" .. object
+res = ngx.location.capture(url)
+if res.status == 404 then
+  ngx.say('OBJECT NOT FOUND')
+  ngx.say('PASSED')
+  ngx.flush(true)
+  return
+elseif res.status == 200 then
+  ngx.say('OBJECT FOUND')
+  ngx.flush(true)
+else
+  ngx.say('INTERNAL ERROR')
+  ngx.say('FAILED')
+  ngx.flush(true)
+  return
+end
+
 -- Check if the versioned object is already present.
 --
 ngx.say('CHECK IF THE OBJECT IS ALREADY VERSIONED...')
 ngx.flush(true)
 url = "/force_real_request/download/" .. build .. "/.ARTIFACTS_BEFORE/" .. version .. "/" .. object
 res = ngx.location.capture(url)
-if res.status == 200 then
-  ngx.say('OBJECT VERSIONED ALREADY')
+if res.status == 404 then
+  ngx.say('VERSIONED OBJECT NOT FOUND')
+  ngx.flush(true)
+elseif res.status == 200 then
+  ngx.say('VERSIONED OBJECT FOUND')
+  ngx.say('PASSED')
   ngx.flush(true)
   return
 else
-  ngx.say('OBJECT NOT VERSIONED YET')
+  ngx.say('INTERNAL ERROR')
+  ngx.say('FAILED')
   ngx.flush(true)
+  return
 end
 
 -- Copy the object to the versioned object location.
@@ -28,11 +55,10 @@ object_url = "/force_real_request/copy/" .. build .. "/" .. build .. "/.ARTIFACT
 object_res = ngx.location.capture(object_url, { method = ngx.HTTP_PUT, body = '' })
 if object_res.status == 200 then
   ngx.say('DONE')
+  ngx.say('PASSED')
   ngx.flush(true)
 else
+  ngx.say('INTERNAL ERROR')
   ngx.say('FAILED')
   ngx.flush(true)
-  return
 end
-
-ngx.say('OBJECT VERSIONED')
