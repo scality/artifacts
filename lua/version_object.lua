c52d6cf35a1cae75a3af84a4165071afaac84102
Bertrand Demiddelaer <bertrand.demiddelaer@scality.com>
2022-04-11 17:47:44 +0200

    RELENG-5710 remove one GCS call

--- lua/version_object.lua
+++ lua/version_object.lua
@@ -5,27 +5,6 @@ local version, build, object = string.match(ngx.var.canonical_path, "^([^/]+)/([
 
 local url, res
 
--- Check if the object is already present.
---
-ngx.say('CHECK IF THE OBJECT IS ALREADY UPLOADED...')
-ngx.flush(true)
-url = "/force_real_request/download/" .. build .. "/" .. object
-res = ngx.location.capture(url)
-if res.status == 404 then
-  ngx.say('OBJECT NOT FOUND')
-  ngx.say('PASSED')
-  ngx.flush(true)
-  return
-elseif res.status == 200 then
-  ngx.say('OBJECT FOUND')
-  ngx.flush(true)
-else
-  ngx.say('INTERNAL ERROR')
-  ngx.say('FAILED')
-  ngx.flush(true)
-  return
-end
-
 -- Check if the versioned object is already present.
 --
 ngx.say('CHECK IF THE OBJECT IS ALREADY VERSIONED...')
@@ -55,6 +34,10 @@ object_url = "/force_real_request/copy/" .. build .. "/" .. build .. "/.ARTIFACT
 object_res = ngx.location.capture(object_url, { method = ngx.HTTP_PUT, body = '' })
 if object_res.status == 200 then
   ngx.say('DONE')
+  ngx.say('PASSED')
+  ngx.flush(true)
+elseif object_res.status == 404 then
+  ngx.say('SOURCE OBJECT NOT FOUND')
   ngx.say('PASSED')
   ngx.flush(true)
 else
