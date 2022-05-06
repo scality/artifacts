97428eb4c9ff02582ae222663354786909263519
Erwan Bernard <erwan.bernard079@gmail.com>
2022-05-06 15:20:54 +0200

    [RELENG-5915] Log body response

--- lua/github_access.lua
+++ lua/github_access.lua
@@ -98,13 +98,8 @@ function authenticate(auth)
                 log_message = log_message .. '(Location: ' .. res.header['Location'] .. ')\n'
             end
             ngx.log(ngx.STDERR, log_message)
-            local zlib = require("zlib")
-            local stream = zlib.inflate()
-            if pcall(function ()  stream(res.body) end) then
-                local inflated_body = stream(res.body)
-                ngx.log(ngx.STDERR, inflated_body)
-            end
-	    return  false
+            pcall(function () ngx.log(ngx.STDERR, require("zlib").inflate()(res.body)) end)
+            return  false
         end
         return true
     else
