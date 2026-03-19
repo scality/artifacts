468328069d19a5f934462581860aca70b87603a1
matthiasL-scality <matthias.lachevre@scality.com>
2026-03-19 14:43:06 +0100

    Redirect upload to s3

--- lua/compute_aws_s3_signature.lua
+++ lua/compute_aws_s3_signature.lua
@@ -323,6 +323,60 @@ elseif signature_mode == "MULTIPART_ABORT" then
     "/" .. ngx.var.aws_tgt_bucket .. "/" .. ngx.var.encoded_key .. "?uploadId=" .. upload_id
   )
 
+elseif signature_mode == "PRESIGN_PUT" then
+
+  -- PRESIGN_PUT: return a presigned S3 PUT URL for a single-file upload.
+  -- The client (GitHub Actions runner) GETs this endpoint to obtain a
+  -- presigned URL, then PUTs the file body directly to S3, bypassing nginx.
+  --
+  ngx.var.encoded_key = get_encoded_key(ngx.var.canonical_path)
+  local build_tgt = ngx.var.canonical_path:match("^[^/]+")
+  scan_tgt_buckets(build_tgt)
+
+  local expires = ngx.time() + 3600
+  local aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+  local canonicalized_resource = "/" .. ngx.var.aws_tgt_bucket .. "/" .. ngx.var.encoded_key
+  -- Sign as PUT with no Content-MD5 and no Content-Type (presigned, not proxied).
+  local string_to_sign = "PUT\n\n\n" .. expires .. "\n" .. canonicalized_resource
+  local aws_signature = ngx.encode_base64(ngx.hmac_sha1(aws_secret_key, string_to_sign))
+  local presigned_url = ngx.var.redirect_endpoint .. canonicalized_resource .. "?" ..
+    ngx.encode_args({AWSAccessKeyId = ngx.var.aws_access_key, Expires = expires, Signature = aws_signature})
+
+  ngx.header.content_type = "text/plain"
+  ngx.say(presigned_url)
+  return ngx.exit(ngx.HTTP_OK)
+
+elseif signature_mode == "PRESIGN_PART" then
+
+  -- PRESIGN_PART: return a presigned S3 PUT URL for one multipart part.
+  -- The client GETs ?partNumber=N&uploadId=X to obtain a presigned URL,
+  -- then PUTs the part body directly to S3.
+  --
+  ngx.var.encoded_key = get_encoded_key(ngx.var.canonical_path)
+  if ngx.var.aws_tgt_bucket == "" then
+    ngx.var.aws_tgt_bucket = aws_bucket_prefix .. "-staging"
+  end
+
+  local part_number = ngx.var.arg_partNumber or ""
+  local upload_id   = ngx.var.arg_uploadId   or ""
+  local expires     = ngx.time() + 3600
+  local aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+  -- subresources must appear in canonical resource (alphabetical: partNumber < uploadId)
+  local canonicalized_resource = "/" .. ngx.var.aws_tgt_bucket .. "/" .. ngx.var.encoded_key ..
+    "?partNumber=" .. part_number .. "&uploadId=" .. upload_id
+  local string_to_sign = "PUT\n\n\n" .. expires .. "\n" .. canonicalized_resource
+  local aws_signature = ngx.encode_base64(ngx.hmac_sha1(aws_secret_key, string_to_sign))
+  local presigned_url = ngx.var.redirect_endpoint .. "/" .. ngx.var.aws_tgt_bucket .. "/" .. ngx.var.encoded_key ..
+    "?partNumber=" .. part_number ..
+    "&uploadId=" .. ngx.escape_uri(upload_id) ..
+    "&AWSAccessKeyId=" .. ngx.escape_uri(ngx.var.aws_access_key) ..
+    "&Expires=" .. expires ..
+    "&Signature=" .. ngx.escape_uri(aws_signature)
+
+  ngx.header.content_type = "text/plain"
+  ngx.say(presigned_url)
+  return ngx.exit(ngx.HTTP_OK)
+
 else
 
   --
